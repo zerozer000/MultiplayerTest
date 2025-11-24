be277cca85621ef686eb0d119b32f5eebc3a80e4
zero000zer <115824640+zerozer000@users.noreply.github.com>
2025-11-25 02:42:29 +0300

    Added source files

---
+++ MultiplayerTCPSocketsTestClient/Client.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Sockets;
+
+using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+try
+{
+    await socket.ConnectAsync("127.0.0.1", 7777);
+    Console.WriteLine($"Connected to {socket.RemoteEndPoint}");
+}
+catch (SocketException)
+{
+    Console.WriteLine($"Cannot connect to {socket.RemoteEndPoint}");
+}
---
+++ MultiplayerTCPSocketsTestServer/Server.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 7777);
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket.Bind(ipPoint);
+socket.Listen();
+Console.WriteLine("Server started waiting for connections");
+// получаем входящее подключение
+using Socket client = await socket.AcceptAsync();
+// получаем адрес клиента
+Console.WriteLine($"Client connected: {client.RemoteEndPoint}");
