a9344996396c2a728eddfe4b9b104741b61022dc
zero <breanvreal@gmail.com>
2025-11-25 04:52:58 +0300

    minor update

--- MultiplayerTCPSocketsTestServer/Server.cs
+++ MultiplayerTCPSocketsTestServer/Server.cs
@@ -15,7 +15,7 @@ using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, 
 socket.Bind(ipPoint);
 socket.Listen();
 Console.WriteLine("Server started waiting for connections");
-// получаем входящее подключение
+
 using Socket client = await socket.AcceptAsync();
-// получаем адрес клиента
+
 Console.WriteLine($"Client connected: {client.RemoteEndPoint}");
